a05cc3ea2370ed3fe2a477bca3ceea4e3f07eb05
bintao <bintao@cteemo.com>
2015-01-26 15:07:34 -0600

    Cteemo_LoginUI
    :D

---
+++ Cteemo_test/Cteemo/Login_CreateViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Login_CreateViewController.swift
+//  Cteemo
+//
+//  Created by Kedan Li on 15/1/24.
+//  Copyright (c) 2015年 Kedan Li. All rights reserved.
+//
+
+import UIKit
+
+class Login_CreateViewController: UIViewController {
+    
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    @IBOutlet weak var createUser: UIButton!
+    
+    
+    
+}
