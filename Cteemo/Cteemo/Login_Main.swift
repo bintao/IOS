5579f46c21aa6dac97dc35e64ff0747d0b273e69
bintao <bintao@cteemo.com>
2015-01-26 21:44:41 -0600

    delete

--- Cteemo/Cteemo/Login_Main.swift
+++ Cteemo/Cteemo/Login_Main.swift
@@ -8,11 +8,7 @@
 
 import UIKit
 
-<<<<<<< HEAD:Cteemo/Cteemo/Login_MainViewController.swift
-class Login_MainViewController: UIViewController, FBLoginViewDelegate, UITextFieldDelegate{
-=======
 class Login_Main: UIViewController, FBLoginViewDelegate{
->>>>>>> bintao:Cteemo/Cteemo/Login_Main.swift
 
     @IBOutlet var bg : UIImageView!
 
@@ -34,72 +30,13 @@ class Login_Main: UIViewController, FBLoginViewDelegate{
         loginView.frame.size = facebook.frame.size
         self.facebook.addSubview(loginView)
         
-//
-        //add tap gesture to board
-        self.bg.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "keyboardReturn:"))
-
         // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    @IBAction func loginWithUserAndPass(){
-        if (email.text != nil && email.text.rangeOfString("@")?.isEmpty != nil) && password.text != nil{
-            //login
-        }else{
-            //login failed
-            println("invalid")
-        }
     }
     
     func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
         
     }
     
-<<<<<<< HEAD:Cteemo/Cteemo/Login_MainViewController.swift
-    // keyboard customization
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        if textField == email{
-            email.resignFirstResponder()
-            password.becomeFirstResponder()
-        }else if textField == password{
-            password.resignFirstResponder()
-            loginWithUserAndPass()
-        }
-        return true
-    }
-    
-    // keyboard return
-    func keyboardReturn(gestureRecognizer: UITapGestureRecognizer){
-        password.resignFirstResponder()
-        email.resignFirstResponder()
-    }
-
-    
-    // get facebook portrait
-    func getPotraitFromFacebook()->UIImage{
-        
-        var image:UIImage!
-        FBRequest.requestForMe().startWithCompletionHandler({(connection: FBRequestConnection!, user: AnyObject!, error: NSError!) -> Void in
-            if((error) != nil){
-                //error
-            }else{
-                println(user as [String: AnyObject])
-                
-                var userID = user["id"] as String
-                var str = "http://graph.facebook.com/\(userID)/picture?type=large"
-                var url = NSURL(string: str)
-                println(url)
-                var data: NSData = NSData(contentsOfURL: url! as NSURL, options: nil, error: nil)!
-                image = UIImage(data: data)
-                
-            }
-        })
-        
-        return image
-        
-    }
-    
-=======
->>>>>>> bintao:Cteemo/Cteemo/Login_Main.swift
     func loginView(loginView: FBLoginView!, handleError error: NSError!) {
         //println(error)
     }
