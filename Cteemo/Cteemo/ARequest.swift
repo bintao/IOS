2a98abb4b6d3285cd95879b255fdfb05aa7f5139
likedan <likedan5@icloud.com>
2015-01-29 22:12:08 -0600

    request delegate

--- Cteemo/Cteemo/ARequest.swift
+++ Cteemo/Cteemo/ARequest.swift
@@ -9,19 +9,39 @@
 
 import UIKit
 
+protocol ResultDelegate:NSObjectProtocol{
+    func gotResult(prefix:String ,result: [String: AnyObject])
+}
+
+//a request class
 class ARequest: NSObject {
     
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-    var info: [String: AnyObjects]!
+    var delegate:ResultDelegate!
     
-=======
-<<<<<<< Updated upstream
->>>>>>> FETCH_HEAD
-    
-=======
-=======
->>>>>>> Stashed changes
+    var info: [String: AnyObject]!
     var method: String!
+    var prefix: String!
+    var returnView: UIViewController!
+    var result: [String: AnyObject]!
     
+    init(prefix: String, method: String, data: [String: AnyObject], returnView: UIViewController){
+        super.init()
+        self.method = method
+        self.info = data
+        self.returnView = returnView
+        self.prefix = prefix
+        
+    }
+    
+    func sendRequest(){
+        InteractingWithServer.connectASynchoronous(self.prefix, info: self.info, method: self.method, theRequest:self)
+    }
+    
+    func gotResult(result: [String: AnyObject]){
+        
+        self.result = result
+        
+        self.delegate.gotResult(self.prefix, result: result)
+    }
+
 }
