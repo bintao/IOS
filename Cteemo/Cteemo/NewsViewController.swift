abd9a0d8563175faa55022d01db4bd0ddfce176a
yuwang17 <yuwang17@illinois.edu>
2015-02-09 23:42:17 -0600

    News View prototype has been added

--- Cteemo/Cteemo/NewsViewController.swift
+++ Cteemo/Cteemo/NewsViewController.swift
@@ -8,19 +8,72 @@
 
 import UIKit
 
-class NewsViewController: UIViewController {
+class NewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var imageScroll : UIScrollView!
+    @IBOutlet var newsTable : UITableView!
+    @IBOutlet var menu: UIBarButtonItem!
+
+    //@IBOutlet var menu : UIBarButtonItem!
+    var hotImageIcon : UIButton!
+    
+    
+    var newsArr: [AnyObject] = ["news1", "news2", "news3", "news4"]
+    var hotImage = UIImageView(image: UIImage(named: "RED"))
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        /*
+        // Hot Image Icon
+        // Icon frame
+        hotImageIcon = UIButton(frame: CGRectMake(45, 18, 70, 70))
+        hotImageIcon.setImage(UIImage(named: "RED"), forState: UIControlState.Normal)
+        // text frame
+        var imageText = UILabel(frame: CGRectMake(12, 25, 80, 20))
+        // text attributes
+        imageText.font = UIFont(name: "AvenirNext-Bold", size: 22)
+        imageText.textColor = UIColor.whiteColor()
+        imageText.text = "HOT"
+        // addsubview
+        self.view.addSubview(hotImageIcon)
+        hotImageIcon.addSubview(imageText)
+        */
         // Do any additional setup after loading the view.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func clickMenu(sender: AnyObject) {
+        
+    }
+    
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newsArr.count
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        var cellImage = UIImageView(frame: CGRectMake(0, 10, 125, 85))
+        cellImage.backgroundColor = UIColor.darkGrayColor()
+        cell.addSubview(cellImage)
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
