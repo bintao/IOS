33172d1cba286b554f1b21e18b51101f5d3e47a8
bintao <bintao@cteemo.com>
2015-03-09 00:29:51 -0500

    asd

--- Cteemo/Cteemo/AppDelegate.swift
+++ Cteemo/Cteemo/AppDelegate.swift
@@ -5,7 +5,96 @@
 //  Created by Kedan Li on 15/1/24.
 //  Copyright (c) 2015年 Kedan Li. All rights reserved.
 //
+/*
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+// Override point for customization after application launch.
 
+// 初始化 SDK，传入 App Key，deviceToken 暂时为空，等待获取权限。
+[RCIM initWithAppKey:@"e7x8xycsx6flq" deviceToken:nil];
+
+#ifdef __IPHONE_8_0
+// 在 iOS 8 下注册苹果推送，申请推送权限。
+UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeBadge
+|UIUserNotificationTypeSound
+|UIUserNotificationTypeAlert) categories:nil];
+[[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+#else
+// 注册苹果推送，申请推送权限。
+[[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound];
+#endif
+
+// 初始化 ViewController。
+ViewController *viewController = [[ViewController alloc]initWithNibName:nil bundle:nil];
+
+// 初始化 UINavigationController。
+UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:viewController];
+
+// 设置背景颜色为黑色。
+[nav.navigationBar setBackgroundColor:[UIColor blackColor]];
+
+// 初始化 rootViewController。
+self.window.rootViewController = nav;
+
+self.window.backgroundColor = [UIColor whiteColor];
+[self.window makeKeyAndVisible];
+return YES;
+}
+
+#ifdef __IPHONE_8_0
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+// Register to receive notifications.
+[application registerForRemoteNotifications];
+}
+
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void(^)())completionHandler
+{
+// Handle the actions.
+if ([identifier isEqualToString:@"declineAction"]){
+}
+else if ([identifier isEqualToString:@"answerAction"]){
+}
+}
+#endif
+
+// 获取苹果推送权限成功。
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+// 设置 deviceToken。
+[[RCIM sharedRCIM] setDeviceToken:deviceToken];
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
+
+
+*/
 import UIKit
 import CoreData
 
@@ -27,12 +116,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             var types: UIUserNotificationType = UIUserNotificationType.Badge |
                 UIUserNotificationType.Alert |
                 UIUserNotificationType.Sound
-            
-        
         
         application.registerForRemoteNotifications()
-        
-        var viewController: ViewController =
         
         // Override point for customization after application launch.
         return true
