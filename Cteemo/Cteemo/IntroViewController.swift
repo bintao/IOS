d239f75df0a39091120baeaa89e179eba930dc41
likedan <likedan5@icloud.com>
2015-04-08 21:00:33 -0500

    upload

--- Cteemo/Cteemo/IntroViewController.swift
+++ Cteemo/Cteemo/IntroViewController.swift
@@ -47,17 +47,20 @@ class IntroViewController: UIViewController, UIScrollViewDelegate{
         secondView = NSBundle.mainBundle().loadNibNamed("2View", owner: self, options: nil)[0] as second
         secondView.center.x = secondView.center.x + view.frame.width
         scroller.addSubview(secondView)
-        secondView.toInitialState()
         
         thirdView = NSBundle.mainBundle().loadNibNamed("3View", owner: self, options: nil)[0] as third
         thirdView.center.x = thirdView.center.x + view.frame.width * 2
         scroller.addSubview(thirdView)
         
         thirdView.toInitialState()
-        
+        scroller.backgroundColor = UIColor(red: 213/255.0, green: 231/255.0, blue: 239/255.0, alpha: 1)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        firstView.setup()
+        secondView.setup()
+    }
     
     func dragged(recognizer : UIPanGestureRecognizer) {
         
@@ -78,7 +81,7 @@ class IntroViewController: UIViewController, UIScrollViewDelegate{
         if index >= 0 && index < view.frame.width * 2{
             secondView.moveEverythingAccordingToIndex(index)
         }else if index < 0{
-            secondView.toInitialState()
+
         }
         if index >= view.frame.width && index < view.frame.width * 3{
             thirdView.moveEverythingAccordingToIndex(index - view.frame.width)
@@ -110,51 +113,54 @@ class IntroViewController: UIViewController, UIScrollViewDelegate{
 
 class first: UIView{
     
-    @IBOutlet var lab1: UILabel!
-    @IBOutlet var lab2: UILabel!
-    @IBOutlet var lab3: UILabel!
-    @IBOutlet var lab4: UILabel!
-    @IBOutlet var lab5: UILabel!
-    @IBOutlet var lab6: UILabel!
     
-    @IBOutlet var icon: UIImageView!
+    @IBOutlet var cloudUp: UIImageView!
+    @IBOutlet var cloudDown: UIImageView!
+    @IBOutlet var circle: UIImageView!
+    @IBOutlet var lab: UIImageView!
     
-    @IBOutlet var iconBack: UIImageView!
-    @IBOutlet var iconFront: UIImageView!
+    
+    func setup() {
+        
+        circle.frame.size = CGSizeMake(superview!.frame.width / 1.2, superview!.frame.width / 1.2)
+        circle.center = self.superview!.center
+        
+        cloudUp.frame.size = CGSizeMake(superview!.frame.width / 1.6, superview!.frame.width / 2)
+        cloudUp.center = CGPointMake(self.frame.width, cloudDown.frame.height / 2 + 20)
+        cloudDown.frame.size = CGSizeMake(superview!.frame.width / 2, superview!.frame.width / 2.5)
+        cloudDown.center = CGPointMake(circle.frame.origin.x, circle.frame.origin.y)
+        
+        lab.frame.size = CGSizeMake(circle.frame.width, circle.frame.width / 5)
+        lab.center = CGPointMake(circle.center.x, circle.frame.origin.y + circle.frame.height + 10 + lab.frame.height / 2)
+        
+    }
     
     func moveEverythingAccordingToIndex(index: CGFloat){
         
         var stay = CGAffineTransformMakeTranslation(index, 0)
         var up = CGAffineTransformMakeTranslation(index, -index / 3)
         var down = CGAffineTransformMakeTranslation(index, index / 3)
-        var speed1 = CGAffineTransformMakeTranslation(-index / 5, -index / 5)
-        var speed2 = CGAffineTransformMakeTranslation(index / 4, -index / 10)
-        var speed3 = CGAffineTransformMakeTranslation(-index / 5, 0)
         var speed4 = CGAffineTransformMakeTranslation(index / 4, index / 10)
         var speed5 = CGAffineTransformMakeTranslation(-index / 5, index / 5)
-        var enlarge = CGAffineTransformMake(1 + index / 20, 0, 0, 1 + index / 20, index, 0)
         
-        lab1.transform = down
-        lab1.alpha = (200 - index) / 200
+
+        var speed1 = CGAffineTransformMakeTranslation(-index / 5, 0)
+        cloudDown.transform = speed1
         
-        icon.transform = up
-        icon.alpha = (200 - index) / 200
+        var speed2 = CGAffineTransformMakeTranslation(index / 3, 0)
+        cloudUp.transform = speed2
         
-        lab2.transform = speed1
+        var speed3 = CGAffineTransformMake(1 - index / 200, 0, 0, 1 - index / 200, 0, index * index / 1000)
+        lab.transform = speed3
         
-        lab3.transform = speed2
-        
-        lab4.transform = speed3
-        
-        lab5.transform = speed4
-        
-        lab6.transform = speed5
-        
-        if index < 250{
-            iconBack.transform = enlarge
-            iconBack.alpha = 1
+        if index <= self.frame.width / 2{
+            superview!.backgroundColor = UIColor(red: 213/255.0, green: 231/255.0, blue: 239/255.0, alpha: 1)
+        }else if index > self.frame.width / 2 && index < self.frame.width - 10{
+            let total = self.frame.width / 2 - 10
+            let current = index - self.frame.width / 2
+            superview!.backgroundColor = UIColor(red: (213 - current / total * 80) / 255.0, green: (231 - current / total * 44)/255.0, blue: (239 - current / total * 23)/255.0, alpha: 1)
         }else{
-            iconBack.alpha = 0
+            superview!.backgroundColor = UIColor(red: 133/255.0, green: 187/255.0, blue: 216/255.0, alpha: 1)
         }
         
     }
@@ -163,134 +169,104 @@ class first: UIView{
 
 class second: UIView{
     
-    //0 - 640
     
-    @IBOutlet var words: UIView!
-    @IBOutlet var redo1: UILabel!
-    @IBOutlet var redo2: UILabel!
-    @IBOutlet var board: UIView!
-    
-    @IBOutlet var choice1: UIImageView!
-    @IBOutlet var choice2: UIImageView!
-    @IBOutlet var choice3: UIImageView!
-    @IBOutlet var choice4: UIImageView!
-    @IBOutlet var choice5: UIImageView!
-    @IBOutlet var choice6: UIImageView!
-    @IBOutlet var choice7: UIImageView!
-    @IBOutlet var choice8: UIImageView!
-    @IBOutlet var choice9: UIImageView!
-    
-    func toInitialState(){
+    @IBOutlet var circle: UIImageView!
+    @IBOutlet var cloudUp: UIImageView!
+    @IBOutlet var cloudDown: UIImageView!
+    @IBOutlet var cloudDown2: UIImageView!
+    @IBOutlet var lab: UIImageView!
+    @IBOutlet var star: UIImageView!
+    @IBOutlet var icon1: UIImageView!
+    @IBOutlet var icon2: UIImageView!
+
+    func setup(){
         
         self.backgroundColor = UIColor.clearColor()
         
-        var enlarge = CGAffineTransformMake(20, 0, 0, 20, 0, 0)
+        cloudUp.frame.size = CGSizeMake(superview!.frame.width / 2.5, superview!.frame.width / 3)
+        cloudUp.center = CGPointMake(self.frame.width, cloudDown.frame.height / 2)
+        cloudDown.frame.size = CGSizeMake(superview!.frame.width / 3, superview!.frame.width / 5)
+        cloudDown.center = CGPointMake(0, frame.height - cloudDown.frame.height / 2 - 20)
+        cloudDown2.frame.size = CGSizeMake(superview!.frame.width / 3, superview!.frame.width / 5)
+        cloudDown2.center = CGPointMake(frame.width, frame.height - cloudDown.frame.height / 2 - 20)
         
-        redo1.alpha = 0
-        redo2.alpha = 0
+        circle.frame.size = CGSizeMake(superview!.frame.width / 1.2, superview!.frame.width / 1.2)
+        circle.center = self.superview!.center
         
-        redo1.transform = enlarge
-        redo2.transform = enlarge
+        star.frame = CGRectMake(circle.frame.origin.x , circle.frame.origin.y - 80 + circle.frame.height, 80, 80)
         
-        choice1.alpha = 0
-        choice2.alpha = 0
-        choice3.alpha = 0
-        choice4.alpha = 0
-        choice5.alpha = 0
-        choice6.alpha = 0
-        choice7.alpha = 0
-        choice8.alpha = 0
-        choice9.alpha = 0
+        lab.frame.size = CGSizeMake(circle.frame.width, circle.frame.width / 5)
+        lab.center = CGPointMake(circle.center.x, circle.frame.origin.y + circle.frame.height + 10 + lab.frame.height / 2)
+        lab.transform = CGAffineTransformMake(0, 0, 0, 0, 0, 1000)
         
-        words.alpha = 0
-        
-        var shrinkBoard = CGAffineTransformMake(1 / 640, 0, 0, 1 / 640, -320, board.frame.height)
-        board.transform = shrinkBoard
+        icon1.frame = CGRectMake(circle.frame.origin.x, circle.frame.origin.y + circle.frame.height / 4, circle.frame.width / 4, circle.frame.width / 4)
+        icon1.alpha = 0
+        icon2.frame = CGRectMake(circle.center.x + circle.frame.height / 4, circle.center.y, circle.frame.width / 4, circle.frame.width / 4)
+        icon2.alpha = 0
+
     }
+    
+    //0 - width * 2
     
     func moveEverythingAccordingToIndex(index: CGFloat){
         
+        var speed1 = CGAffineTransformMakeTranslation(-index / 5, 0)
+        cloudDown.transform = speed1
         
+        var speed2 = CGAffineTransformMakeTranslation(index / 2, 0)
+        cloudUp.transform = speed2
         
-        if index < 160{
-            var enlarge = CGAffineTransformMake(4 * index / 640, 0, 0, 4 * index / 640, -320 + index, board.frame.height - index * board.frame.height / 160)
-            board.transform = enlarge
-        }else if index >= 160 && index <= 320{
-            var enlarge = CGAffineTransformMake(1, 0, 0, 1, -320 + index, 0)
-            board.transform = enlarge
-        }else if index > 320 && index < 480{
-            var turnLeft = CGAffineTransformMakeRotation((index - 320) * 3.14 / 320)
-            board.transform = turnLeft
-        }else if index >= 480{
-            var turnLeft = CGAffineTransformMakeRotation(3.14 / 2)
-            board.transform = turnLeft
-        }
-        
-        
-        if index > 160{
-            choice1.alpha = (index - 160) / 10
-            choice2.alpha = (index - 170) / 10
-            choice3.alpha = (index - 180) / 10
-            choice4.alpha = (index - 190) / 10
-            choice5.alpha = (index - 200) / 10
-            choice6.alpha = (index - 210) / 10
-            choice7.alpha = (index - 220) / 10
-            choice8.alpha = (index - 230) / 10
-            choice9.alpha = (index - 240) / 10
+        if index <= self.frame.width / 2{
+            lab.transform = CGAffineTransformMake(0, 0, 0, 0, 0, 1000)
+        }else if index > self.frame.width / 2 && index < self.frame.width{
+            let total = self.frame.width / 2
+            let current = index - self.frame.width / 2
+            lab.transform = CGAffineTransformMake(current / total, 0, 0, current / total, 0, 1 / (current / total) / (current / total))
+        }else if index <= self.frame.width{
+            lab.transform = CGAffineTransformMake(1, 0, 0, 1, 0, 0)
+        }else if index > self.frame.width && index <= self.frame.width * 1.5{
+            let total = self.frame.width * 0.5
+            let current = index - self.frame.width
+            lab.transform = CGAffineTransformMake(1 - current / total, 0, 0, 1 - current / total, 0, 1 / ( 1 - current / total) / (1 - current / total))
         }else{
-            choice1.alpha = 0
-            choice2.alpha = 0
-            choice3.alpha = 0
-            choice4.alpha = 0
-            choice5.alpha = 0
-            choice6.alpha = 0
-            choice7.alpha = 0
-            choice8.alpha = 0
-            choice9.alpha = 0
+            lab.transform = CGAffineTransformMake(0, 0, 0, 0, 0, 1000)
         }
         
-        if index <= 230{
-            redo1.alpha = 0
-        } else if index > 230 && index < 270{
-            var shrink = CGAffineTransformMake((271 - index) / 2, 0, 0, (271 - index) / 2, 0, 0)
-            redo1.alpha = (index - 230) / 40
-            redo1.transform = shrink
-        } else if index >= 270 && index <= 320 {
-            redo1.alpha = 1
-            var shrink = CGAffineTransformMake(1, 0, 0, 1, 0, 0)
-            redo1.transform = shrink
-        }
         
-        var speed1 = CGAffineTransformMakeTranslation(-(index - 320)/4, 0)
-        var speed2 = CGAffineTransformMakeTranslation((index - 320)/4, 0)
-        
-        if index <= 260 {
-            redo2.alpha = 0
-        } else if index > 260 && index < 300{
-            var shrink = CGAffineTransformMake((301 - index) / 2, 0, 0, (301 - index) / 2, 0, 0)
-            redo2.alpha = (index - 260) / 40
-            redo2.transform = shrink
-        }else if index >= 300 && index <= 320 {
-            redo2.alpha = 1
-            var shrink = CGAffineTransformMake(1, 0, 0, 1, 0, 0)
-            redo2.transform = shrink
-        }else if index > 320{
-            redo2.transform = speed1
-        }
-        
-        if index < 320{
-            words.alpha = index / 320
+        if index <= self.frame.width * 0.5{
+            star.transform = CGAffineTransformMakeScale(0, 0)
+        }else if index > self.frame.width * 0.5 && index <= self.frame.width * 0.7{
+            let total = self.frame.width * 0.2
+            let current = index - self.frame.width * 0.5
+            star.transform = CGAffineTransformMakeScale(current / total * 1.2, current / total * 1.2)
+        }else if index > self.frame.width * 0.7 && index <= self.frame.width * 0.8{
+            let total = self.frame.width * 0.1
+            let current = index - self.frame.width * 0.7
+            star.transform = CGAffineTransformMakeScale(1.2 - current / total * 0.2, 1.2 - current / total * 0.2)
         }else{
-            words.alpha =  1 - (index - 320) / 320
-            words.transform = speed2
-        }
-        if index <= 231{
-            self.superview?.backgroundColor = UIColor.clearColor()
-        }else if index > 231 && index < 320{
-            self.superview?.backgroundColor = UIColor(red: 32.0/255, green: 176.0/255, blue: 140.0/255, alpha: 1)
-            
+            star.transform = CGAffineTransformMakeScale(1, 1)
         }
         
+        if index <= self.frame.width * 0.7{
+            icon1.alpha = 0
+        }else if index > self.frame.width * 0.7 && index <= self.frame.width * 0.8{
+            let total = self.frame.width * 0.1
+            let current = index - self.frame.width * 0.7
+            icon1.alpha = current / total
+        }else{
+            icon1.alpha = 1
+        }
+        
+        if index <= self.frame.width * 0.8{
+            icon2.alpha = 0
+        }else if index > self.frame.width * 0.8 && index <= self.frame.width * 0.9{
+            let total = self.frame.width * 0.1
+            let current = index - self.frame.width * 0.8
+            icon2.alpha = current / total
+        }else{
+            icon2.alpha = 1
+        }
+
     }
     
 }
@@ -311,57 +287,9 @@ class third: UIView{
         
     }
     
-    //0 - 640
+    // width - width * 3
     func moveEverythingAccordingToIndex(index: CGFloat){
         
-        var stay = CGAffineTransformMakeTranslation(index, 0)
-        var turn = CGAffineTransformMake(1 - (index - 250) / 70, (index - 250) / 70 - (index - 250) / 210, -(index - 250) / 70 + (index - 250) / 210, 1 - (index - 250) / 70, (index - 250), (index - 250) * 2.0)
-        var transform = CGAffineTransformMake(1 - (index - 250) / 210, 0, 0, 1 - (index - 250) / 210, -(index - 250) / 2, -(index - 250) / 3)
-        
-        var enlarge = CGAffineTransformMake(1 + index / 20, 0, 0, 1 + index / 20, index, 0)
-        
-        if index < 100{
-            self.superview?.backgroundColor = UIColor(red: 32.0/255, green: 176.0/255, blue: 140.0/255, alpha: 1)
-        }else if index > 100 && index < 200{
-            self.superview?.backgroundColor = UIColor(red: (32.0 + (index - 100) * 1.11)/255, green: (176.0 + (index - 100) * 0.29)/255, blue: (140.0 + (index - 100) * 0.67)/255, alpha: 1)
-        }else if index >= 200{
-            self.superview?.backgroundColor = UIColor(red: 143.0/255, green: 205.0/255, blue: 232.0/255, alpha: 1)
-        }
-        
-        if index < 200{
-            vertical.alpha = 0
-            horizontal.alpha = 1
-            vertical.transform = CGAffineTransformMake(1, 0, 0, 1, 0, 0)
-            horizontal.transform = CGAffineTransformMake(1, 0, 0, 1, 0, 0)
-        }else if index > 250 && index <= 320 {
-            vertical.transform = turn
-            vertical.alpha = (index - 250) / 70
-            horizontal.transform = transform
-            portrait.alpha = (index - 250) / 70
-            landscape.alpha = (index - 250) / 70
-            
-        }else if index > 320{
-            vertical.transform = CGAffineTransformMake(0, 0.6777777, -0.6777777, 0, 70, 140 + -(index - 320) * (index - 320) / 30)
-            horizontal.transform = CGAffineTransformMake(0.6777777, 0, 0, 0.6777777, -35, -23.333 + (index - 320) * (index - 320) / 20)
-            
-            vertical.alpha =  1 - (index - 320) / 100
-            horizontal.alpha = 1 - (index - 320) / 100
-            portrait.alpha = 1 - (index - 320) / 100
-            landscape.alpha = 1 - (index - 320) / 100
-        }
-        
-        if index <= 280{
-            lab1.transform = CGAffineTransformMake(1, 0, 0, 1, 0, 0)
-            lab1.alpha = 1
-        }else if index > 280 && index < 320{
-            lab1.transform = CGAffineTransformMake(1 + (index - 280) / 100, 0, 0, 1 + (index - 280) / 100, 0, index - 280)
-            lab1.alpha = 1
-        }else if index >= 320 {
-            lab1.transform = CGAffineTransformMake(1.4 - (index - 320) / 70, 0, 0, 1.4 - (index - 320) / 70, -(index - 320) * 3, 40 + (index - 320) * (index - 320) / 20)
-            lab1.alpha = (420 - index) / 100
-        }
-        
-        println(index)
         
     }
     
